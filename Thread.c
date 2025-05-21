4a703af643360d844f3b932161e5d8e2724297f5
Francesco Corsa <123736789+SonCorsa@users.noreply.github.com>
2025-05-21 21:08:07 +0000

    sistemato

--- Thread.c
+++ Thread.c
@@ -1,45 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include <pthread.h>
+#include <time.h>
 
-#define N 3 // Numero di processi figli da creare
+#define DIMENSIONE 10
+
+int array[DIMENSIONE];
+int conteggio_dispari = 0;
+pthread_mutex_t lock;
+
+void* genera_numeri(void* arg) {
+    srand(time(NULL));
+    conteggio_dispari = 0;
+
+    for (int i = 0; i < DIMENSIONE; i++) {
+        array[i] = rand() % 100;
+        if ((array[i] % 2 != 0) {
+            conteggio_dispari++;
+        }
+    }
+
+    printf("Numeri generati: ");
+    for (int i = 0; i < DIMENSIONE; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\nConteggio numeri dispari: %d\n", conteggio_dispari);
+
+    pthread_exit(NULL);
+}
+
+void ordina_bubble(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void* ordina_e_stampa(void* arg) {
+    ordina_bubble(array, DIMENSIONE);
+
+    printf("\nPrimi 5 numeri: ");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+
+    pthread_exit(NULL);
+}
 
 int main() {
-    int pid;
+    pthread_t thread1, thread2;
 
-    printf("Processo padre (PID: %d) in esecuzione.\n", getpid());
+    pthread_create(&thread1, NULL, genera_numeri, NULL);
+    pthread_join(thread1, NULL);
 
-    for (int i = 0; i < N; i++) {
-        pid = fork(); // Creazione del processo figlio
-        if (pid < 0) {
-            printf("Errore nella fork");
-            exit(EXIT_FAILURE);
-        }
+    pthread_create(&thread2, NULL, ordina_e_stampa, NULL);
+    pthread_join(thread2, NULL);
 
-        if (pid == 0) {
-            // Codice eseguito solo dal processo figlio
-            printf("Processo figlio %d (PID: %d) creato dal padre (PID: %d).\n", i + 1, getpid(), getppid());
-
-            // Simulazione di un'attività del processo figlio
-            sleep(2); // Ad esempio, un'attività che richiede 2 secondi
-
-            printf("Processo figlio %d (PID: %d) termina.\n", i + 1, getpid());
-            exit(0); // Terminazione del processo figlio
-        }
-    }
-
-    // Codice eseguito solo dal processo padre
-    for (int i = 0; i < N; i++) {
-        int status;
-        int child_pid = wait(&status); // Attesa della terminazione di un figlio
-        if (child_pid > 0) {
-            printf("Processo padre: Figlio con PID %d terminato con stato %d.\n", child_pid, WEXITSTATUS(status));
-        } else {
-            perror("Errore nella wait");
-        }
-    }
-
-    printf("Processo padre (PID: %d) termina.\n", getpid());
     return 0;
 }
